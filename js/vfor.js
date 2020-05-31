e13ace548d8d9c03b2ceb03a9d69773f76f127bc
kanehiroyuu <>
2020-05-31 11:33:30 +0900

    v-if, v-for

---
+++ js/vfor.js
@@ -0,0 +1,13 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        message: 'こんにちわん！',
+        url: 'https://www.yuulinux.tokyo/',
+        toggle: true,
+        languages: [
+            'JavaScript',
+            'PHP',
+            'Python'
+        ]
+    }
+})
---
+++ js/vif.js
@@ -0,0 +1,8 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        message: 'こんにちわん！',
+        url: 'https://www.yuulinux.tokyo/',
+        toggle: true
+    }
+})
